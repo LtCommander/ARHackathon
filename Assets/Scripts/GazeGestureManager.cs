c72afe2ed3396114daffc240a631973f516748eb
Ben <bbickl22@gmail.com>
2017-05-04 17:05:04 -0500

    First Commit

---
+++ Assets/Scripts/GazeGestureManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class GazeGestureManager : MonoBehaviour
+{
+    public static GazeGestureManager Instance { get; private set; }
+
+    // Represents the hologram that is currently being gazed at.
+    public GameObject FocusedObject { get; private set; }
+
+    GestureRecognizer recognizer;
+
+    private MicrophoneManager microphoneManager;
+
+    // Use this for initialization
+    void Awake()
+    {
+        microphoneManager = this.GetComponent<MicrophoneManager>();
+
+        Instance = this;
+
+        // Set up a GestureRecognizer to detect Select gestures.
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += (source, tapCount, ray) =>
+        {
+            // Send an OnSelect message to the focused object and its ancestors.
+            if (FocusedObject != null)
+            {
+                FocusedObject.SendMessageUpwards("OnSelect");
+            }
+        };
+        recognizer.HoldStartedEvent += Recognizer_HoldStartedEvent;
+        recognizer.HoldCompletedEvent += Recognizer_HoldCompletedEvent;
+        recognizer.StartCapturingGestures();
+    }
+
+    private void Recognizer_HoldCompletedEvent(InteractionSourceKind source, Ray headRay)
+    {
+        Debug.Log("recording stop");
+        microphoneManager.StopRecording();
+    }
+
+    private void Recognizer_HoldStartedEvent(InteractionSourceKind source, Ray headRay)
+    {
+        Debug.Log("recording started");
+        microphoneManager.StartRecording();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Figure out which hologram is focused this frame.
+        GameObject oldFocusObject = FocusedObject;
+
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            // If the raycast hit a hologram, use that as the focused object.
+            FocusedObject = hitInfo.collider.gameObject;
+        }
+        else
+        {
+            // If the raycast did not hit a hologram, clear the focused object.
+            FocusedObject = null;
+        }
+
+        // If the focused object changed this frame,
+        // start detecting fresh gestures again.
+        if (FocusedObject != oldFocusObject)
+        {
+            recognizer.CancelGestures();
+            recognizer.StartCapturingGestures();
+        }
+    }
+}
---
+++ Assets/Scripts/InvisibleQuadBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvisibleQuadBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject sphereToSpawn;
+    [SerializeField]
+    private GameObject textToSpawn;
+
+    private GameObject textSpawn;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+       // GameObject newSphere = Instantiate(sphereToSpawn, this.transform.position, this.transform.rotation) as GameObject;
+    }
+
+    public Text SpawnWordObject()
+    {
+        textSpawn = Instantiate(textToSpawn, this.transform.position, this.transform.rotation) as GameObject;
+        textSpawn.transform.SetParent(this.transform);
+
+        return textSpawn.GetComponentInChildren<Text>();
+    }
+
+    public void ReleaseWordObject()
+    {
+        textSpawn.transform.SetParent(null);
+    }
+
+
+}
